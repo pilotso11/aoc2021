6087d6539489d2542d16fdb9d37d992163dd09bd
Seth Osher <seth@oshers.com>
2021-12-09 23:56:15 +0000

    simplified don't panic

--- dont_panic.cpp
+++ dont_panic.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 #include <string>
-using namespace std;string R="RIGHT",L="LEFT",W="WAIT",B="BLOCK",D,m;int main(){int N,w,X,x,U,M,C,c,E,e,i;cin>>N>>w>>U>>X>>x>>U>>U>>M;cin.ignore();int V[N];for(i=0;i<M;i++){cin>>E>>e;cin.ignore();V[E]=e;}while(1){m=W;cin>>C>>c>>D;cin.ignore();if(C==X) {if(D==R&&c>x||D==L&&c<x)m=B;}else if(C!=-1&&(D==R&&c>V[C]||D==L&&c<V[C]))m=B;if(m==W&&(D==R&&c==w-1||D==L&&c==0))m=B;cout<<m<<endl;}}
+using namespace std;
+string R = "RIGHT", L = "LEFT", W = "WAIT", B = "BLOCK", direction, cmd;
+
+int main()
+{
+    int nb_floors,width, nb_rounds,exit_floor,exit_pos,nb_total_clones,nb_additional_elevators,nb_elevators, clone_floor,clone_pos,elevator_floor, elevator_pos;
+    cin >> nb_floors >> width >> nb_rounds >> exit_floor >> exit_pos >> nb_total_clones >> nb_additional_elevators >> nb_elevators;
+    cin.ignore();
+
+    int elevators[nb_floors];
+
+    for (int i = 0; i < nb_elevators; i++)
+    {
+        cin >> elevator_floor >> elevator_pos;
+        cin.ignore();
+        elevators[elevator_floor] = elevator_pos;
+    }
+
+    while (1)
+    {
+        cmd=W;
+        cin >> clone_floor >> clone_pos >> direction;
+        cin.ignore();
+
+        if (clone_floor == exit_floor)
+        {
+            if (direction == R && clone_pos > exit_pos || direction == L && clone_pos < exit_pos)
+                cmd = B;
+        }
+        else if (clone_floor != -1 && (direction == R && clone_pos > elevators[clone_floor] || direction == L && clone_pos < elevators[clone_floor]))
+            cmd = B;
+
+
+        cout << cmd << endl;
+    }
+}
