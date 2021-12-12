2cae89564c90a3e0a11643b97dab3ef4666d4311
Seth Osher <seth@oshers.com>
2021-12-12 12:23:03 +0000

    day12 part2

--- day12.py
+++ day12.py
@@ -13,35 +13,57 @@ with open('day12.input') as f:
 
 print(links)
 
+tries = 0
 paths = []
 
+def dup_lower_in_path(path):
+    count = 0
+    lowers = []
+    for node in path:
+        if node.islower():
+            if node in lowers:
+                return True
+            else:
+                lowers.append(node)
+    return False
+
+
 def descend(node, path):
-    if node in path and node.islower():
-        print('Already visited', node)
-        return 
+    global tries
+    tries += 1
+    if tries % 1000 == 0: print('Tries', tries)
+
     if node == 'start': return
 
-    path.append(node)
     if node == 'end':
-        print('End found')
+        #print('End found')
+        path.append(node)
         p = ','.join(path)
         if p not in paths:
             paths.append(p)
+            if len(paths) % 50 == 0: print('Found', len(paths), 'paths so far')
         path.pop()
         return
 
+    if node in path and node.islower() and dup_lower_in_path(path):
+        #print('Already visited', node)
+        return 
+
+    path.append(node)
     if node in links:
         for link in links[node]:
-            print('Try', path, link)
+            #print('Try', path, link)
             descend(link, path)
     path.pop() 
 
 
 for node in links['start']:
-    print('Starting at',node)
+    #print('Starting at',node)
     path = []
     descend(node, path)
 
+#for p in paths:
+#    print(p)
 print('Found', len(paths), 'paths')
 
 
