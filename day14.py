6abadb1e5c40d85d1054d7528dbb1813f7ec894f
Seth Osher <seth@oshers.com>
2021-12-14 10:23:37 +0000

    day14 part2

--- day14.py
+++ day14.py
@@ -17,9 +17,11 @@ polymer = ''
 p_list = None
 tail = None
 
+last = ''
 pairs = {}
+freq = {}
 
-with open('day14.sample', 'r') as f:
+with open('day14.input', 'r') as f:
     polymer = f.readline().strip()
     for c in polymer:
         if p_list == None:
@@ -28,6 +30,7 @@ with open('day14.sample', 'r') as f:
         else:
             tail.next = Node(c, tail)
             tail = tail.next
+            last = c
     
     f.readline()
     while(True):
@@ -35,6 +38,7 @@ with open('day14.sample', 'r') as f:
         parts = line.split(' ')
         if len(parts) == 3:
             pairs[parts[0]] = parts[2]
+            freq[parts[0]] = 0
         else:
             break
 
@@ -44,33 +48,49 @@ def print_node(node):
         node = node.next
     print()
 
-print_node(p_list)
-print(pairs)
+#print_node(p_list)
+#print(pairs)
 
-
-for i in range(10):
     # do one pass
-    print('pass', i)
-    node = p_list
-    while node != None:
-        next = node.next
-        if node.next != None:
-            pair = node.value + node.next.value
-            if pair in pairs:
-                node.insert_after(Node(pairs[pair], None))
-        node = next
+node = p_list
+while node != None:
+    next = node.next
+    if node.next != None:
+        pair = node.value + node.next.value
+        if pair in pairs:
+            freq[pair] += 1
+    node = next
+
+#print(freq) 
+
+for i in range(40):
+    new_freq = {}
+    for (k,v) in freq.items():
+        if v == 0:
+            continue
+        #print(k,v)
+        pair_l = k[0] + pairs[k]
+        pair_r = pairs[k] + k[1]
+        if pair_l not in new_freq: new_freq[pair_l] = 0
+        if pair_r not in new_freq: new_freq[pair_r] = 0
+        new_freq[pair_l] += v
+        new_freq[pair_r] += v
+    freq = new_freq
+print(freq)
 
 counts = {}
-node = p_list
 total = 0
-while node != None:
-    if node.value not in counts:
-        counts[node.value] = 1
-    else:
-        counts[node.value] += 1
-    total += 1
-    node = node.next
+for (k,v) in freq.items():
+    if v == 0:
+        continue
+    if k[0] not in counts:
+        counts[k[0]] = 0
+    counts[k[0]] += v
+    total += v
 
+counts[last] += 1
+total += 1
+print(counts)
 
 max_key = max(counts, key=counts.get)   
 min_key = min(counts, key=counts.get)
@@ -78,7 +98,6 @@ min_key = min(counts, key=counts.get)
 print('Len:', total)
 print("Max:", max_key, "Min:", min_key)
 print('Answer:', counts[max_key] - counts[min_key])
-
 
 
 
